00695a0b86df918b68c32e248542930da686bb40
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-04-20 18:29:15 -0700

    commit

--- client/src/views/EditView.js
+++ client/src/views/EditView.js
@@ -1,13 +1,41 @@
-import React from "react";
+import { navigate } from "@reach/router";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import DeleteButton from "../components/DeleteButton";
 import Form from "../components/Form";
 
 const EditView = props => {
   const { id } = props;
+  const [athlete, setAthlete] =  useState({});
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/athletes/' + id)
+    .then(response => {
+      setAthlete(response.data)
+      setLoaded(true);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  })
+  const onSubmitHandler = (e, data) => {
+    e.preventDefault();
+    axios.put("http://localhost:8000/api/athletes/" +id, data)
+    .then(response => {
+      navigate('/');
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
   return(
     <div>
-      <h1>We are editing Athlete: {id} </h1>
-      <Form />
+      <h1>Edit {athlete.firstName} {athlete.lastName}'s Information </h1>
+     { loaded &&
+      <Form 
+          onSubmitHandler={onSubmitHandler} 
+      />
+     }
       <DeleteButton />
     </div>
   )
